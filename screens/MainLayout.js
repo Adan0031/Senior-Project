609ae0cb51b001f83b9db4eac98d8bce15f8d935
Alonso <alonsovidal1998@gmail.com>
2021-10-07 17:00:46 -0500

    Removed some SVG code that forgot to delete before commiting

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -40,61 +40,6 @@ import {
 
 
 
-import Svg, { Path } from 'react-native-svg';
-const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
-
-    var isSelected = accessibilityState.selected
-
-    if (isSelected) {
-        return (
-            <View style={{ flex: 1, alignItems: "center" }}>
-                <View style={{ flexDirection: 'row', position: 'absolute', top: 0 }}>
-                    
-                    <View style={{ flex: 1, backgroundColor: COLORS.Orange }}></View>
-                    <Svg
-                        width={70}
-                        height={61}
-                        viewBox="0 0 75 61"
-                    >                      
-                        <Path // This gives the shape of the lower part selected tab
-                            d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
-                            fill={COLORS.Orange}
-                        />
-                    </Svg>
-                    <View style={{ flex: 1, backgroundColor: COLORS.Orange }}></View>
-                </View>
-                <TouchableOpacity // This gives the shape and other details of the selected icon in the home menu
-                    style={{
-                        top: -22.5,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        width: 50,
-                        height: 50,
-                        borderRadius: 25,
-                        backgroundColor: COLORS.Orange
-                    }}
-                    onPress={onPress}
-                >
-                    {children}
-                </TouchableOpacity>
-            </View>
-        )
-    } else {
-        return (
-            <TouchableOpacity // This is the rest of the non-selected tab
-                style={{
-                    flex: 1,
-                    height: 60,
-                    backgroundColor: COLORS.Orange
-                }}
-                activeOpacity={1}
-                onPress={onPress}
-            >
-                {children}
-            </TouchableOpacity>
-        )
-    }
-}
 
 
 

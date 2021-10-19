6d770f4dbcf4482ae8f829c056e6f869223a9d23
Adan <adansalinas0031@gmail.com>
2021-10-18 21:24:34 -0500

    Add Outlook Icon

--- screens/Authentication/SignIn.js
+++ screens/Authentication/SignIn.js
@@ -164,7 +164,7 @@ const SignIn = ({ navigation }) => {
 
             {/* Footer */}
             <View>
-                <TextIconButton
+                {/* <TextIconButton
                     containerStyle={{
                         height: 50,
                         alignItems: 'center',
@@ -182,7 +182,7 @@ const SignIn = ({ navigation }) => {
                         color: COLORS.white
                     }}
                     onPress={() => navigation.replace("Home")}
-                />
+                /> */}
 
                 <TextIconButton
                     containerStyle={{

7ef14d966d6e36b9e902f6b6973e56f7d461d042
Adan <adansalinas0031@gmail.com>
2021-11-03 23:53:44 -0500

    Successfully Test Rendering of Account Screen

--- __tests__/screens/Account.test.js
+++ __tests__/screens/Account.test.js
@@ -2,4 +2,27 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 
 
-import { Account } from "../../screens/Account/Account.js";
+import  Account  from "../../screens/Account/Account.js";
+
+// Test that the Account Screen renders correctly
+describe('Account Screen', () => {
+    it('renders correctly', () => {
+        const navigate = jest.fn();
+        const { getByText } = render(<Account navigation={{ navigate }}/>);
+        const titleElement1 = getByText('Hi Username');
+        const titleElement2 = getByText('Name');
+        const titleElement3 = getByText('Email');
+        const titleElement4 = getByText('Password');
+        const titleElement5 = getByText('Payment');
+
+
+
+
+        
+        expect(titleElement1).toBeTruthy();
+        expect(titleElement2).toBeTruthy();
+        expect(titleElement3).toBeTruthy();
+        expect(titleElement4).toBeTruthy();
+        expect(titleElement5).toBeTruthy();
+    });
+});

8bc978601dfeb6e550f8c2ea96674532778a048e
Adan <adansalinas0031@gmail.com>
2021-11-04 20:14:14 -0500

    Test Account Screen

--- __tests__/screens/Account.test.js
+++ __tests__/screens/Account.test.js
@@ -8,21 +8,45 @@ import  Account  from "../../screens/Account/Account.js";
 describe('Account Screen', () => {
     it('renders correctly', () => {
         const navigate = jest.fn();
-        const { getByText } = render(<Account navigation={{ navigate }}/>);
+        const { getByText } = render(<Account navigation={{ }}/>);
         const titleElement1 = getByText('Hi Username');
         const titleElement2 = getByText('Name');
         const titleElement3 = getByText('Email');
         const titleElement4 = getByText('Password');
         const titleElement5 = getByText('Payment');
+        const titleElement6 = getByText('Log Out');
 
-
-
-
-        
         expect(titleElement1).toBeTruthy();
         expect(titleElement2).toBeTruthy();
         expect(titleElement3).toBeTruthy();
         expect(titleElement4).toBeTruthy();
         expect(titleElement5).toBeTruthy();
+        expect(titleElement6).toBeTruthy();
+    });
+});
+
+// On Pressing Button Links, Navigate to the correct screen
+describe('Account Screen', () => {
+        it('redirects to the correct screen', () => {
+        const navigate = jest.fn();
+        const { getByText } = render(<Account navigation={{ navigate }}/>);
+        const titleElement2 = getByText('Name');
+        const titleElement3 = getByText('Email');
+        const titleElement4 = getByText('Password');
+        const titleElement5 = getByText('Payment');
+        const titleElement6 = getByText('Log Out');
+
+        fireEvent.press(titleElement2);
+        fireEvent.press(titleElement3);
+        fireEvent.press(titleElement4);
+        fireEvent.press(titleElement5);
+        fireEvent.press(titleElement6);
+
+        expect(navigate).toHaveBeenCalledTimes(5);
+        expect(navigate).toHaveBeenCalledWith('Account_name');
+        expect(navigate).toHaveBeenCalledWith('Account_email');
+        expect(navigate).toHaveBeenCalledWith('Account_password');
+        expect(navigate).toHaveBeenCalledWith('Account_payment');
+        expect(navigate).toHaveBeenCalledWith('SignIn');
     });
 });

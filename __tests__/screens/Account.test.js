7928cd238a037c286698c0a42bc0a77c981b7c3a
Adan <adansalinas0031@gmail.com>
2021-11-11 21:16:25 -0600

    Fully Test Account Screen Navigation

--- __tests__/screens/Account.test.js
+++ __tests__/screens/Account.test.js
@@ -40,20 +40,28 @@ describe('Account Screen', () => {
         const titleElement3 = getByText('Email');
         const titleElement4 = getByText('Password');
         const titleElement5 = getByText('Payments');
-
-        const titleElement6 = getByText('Log Out');
+        const titleElement6 = getByText('Order History');
+        const titleElement7 = getByText('Push Notifications');
+        const titleElement8 = getByText('App Feedback');
+        const titleElement9 = getByText('Log Out');
 
         fireEvent.press(titleElement2);
         fireEvent.press(titleElement3);
         fireEvent.press(titleElement4);
         fireEvent.press(titleElement5);
         fireEvent.press(titleElement6);
+        fireEvent.press(titleElement7);
+        fireEvent.press(titleElement8);
+        fireEvent.press(titleElement9);
 
-        expect(navigate).toHaveBeenCalledTimes(5);
-        expect(navigate).toHaveBeenCalledWith('Account_name');
-        expect(navigate).toHaveBeenCalledWith('Account_email');
-        expect(navigate).toHaveBeenCalledWith('Account_password');
-        expect(navigate).toHaveBeenCalledWith('Account_payment');
-        expect(navigate).toHaveBeenCalledWith('SignIn');
+        expect(navigate).toHaveBeenCalledTimes(8);
+        expect(navigate).toHaveBeenNthCalledWith(1, 'Account_name');
+        expect(navigate).toHaveBeenNthCalledWith(2, 'Account_email');
+        expect(navigate).toHaveBeenNthCalledWith(3, 'Account_password');
+        expect(navigate).toHaveBeenNthCalledWith(4, 'Account_payment');
+        expect(navigate).toHaveBeenNthCalledWith(5, 'Account_orders_history_screen');
+        expect(navigate).toHaveBeenNthCalledWith(6, 'App_notifications');
+        expect(navigate).toHaveBeenNthCalledWith(7, 'Account_App_feedback');
+        expect(navigate).toHaveBeenNthCalledWith(8, 'SignIn');
     });
 });
